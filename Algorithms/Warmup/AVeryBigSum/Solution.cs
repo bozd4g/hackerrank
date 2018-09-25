61eccf2dde2d2a7be8663233e30d453afab035ef
Furkan Bozdağ <bozd4g@hotmail.com>
2018-09-25 23:36:06 +0300

    Edit A Big Very Sum.

--- Algorithms/Warmup/AVeryBigSum/Solution.cs
+++ Algorithms/Warmup/AVeryBigSum/Solution.cs
@@ -4,22 +4,20 @@ using System.IO;
 using System.Linq;
 class Solution {
 
-    static void Main(String[] args) {
-        int n = Convert.ToInt32(Console.ReadLine());
-        int[][] a = new int[n][];
-        for(int a_i = 0; a_i < n; a_i++){
-           string[] a_temp = Console.ReadLine().Split(' ');
-           a[a_i] = Array.ConvertAll(a_temp,Int32.Parse);
-        }
-        
-        int first=0;
-        int second=0;
+    static long aVeryBigSum(int n, long[] ar) {
+        long result = 0;
         for(int i=0;i<n;i++)
         {
-            first += a[i][i];
-            second+= a[i][n-i-1];
-        }        
-        
-        Console.WriteLine(Math.Abs(first-second));
+            result += ar[i];
+        }
+        return result;
+    }
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] ar_temp = Console.ReadLine().Split(' ');
+        long[] ar = Array.ConvertAll(ar_temp,Int64.Parse);
+        long result = aVeryBigSum(n, ar);
+        Console.WriteLine(result);
     }
 }
