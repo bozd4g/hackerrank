0c5e3217c3dea8c94d49dc48dfc3713c4cdcaa1e
Furkan Bozdag <bozd4g@hotmail.com>
2019-01-03 19:50:41 +0300

    Add The Hurdle Race.

---
+++ Algorithms/Implementation/TheHurdleRace/Solution.cs
@@ -0,0 +1,39 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+     static int hurdleRace(int k, int[] height)
+     {
+          int m = 0;
+          for (int i = 0; i < height.Length; i++)
+               if (height[i] > m) m = height[i];
+          return k > m ? 0 : (m - k);
+     }
+
+     static void Main(string[] args)
+     {
+          TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+          string[] nk = Console.ReadLine().Split(' ');
+          int n = Convert.ToInt32(nk[0]);
+          int k = Convert.ToInt32(nk[1]);
+          int[] height = Array.ConvertAll(Console.ReadLine().Split(' '), heightTemp => Convert.ToInt32(heightTemp));
+          int result = hurdleRace(k, height);
+
+          textWriter.WriteLine(result);
+
+          textWriter.Flush();
+          textWriter.Close();
+     }
+}
